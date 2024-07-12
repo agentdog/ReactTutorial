9608206ec188ad12f13bbd008583e81885bd3138
Valentina Banner <78287030+realhuman101@users.noreply.github.com>
2024-07-12 13:31:36 +0800

    add other modded files

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Header from './Header.jsx'
 import Footer from './Footer.jsx'
+import List from './List.jsx'
 
 function App() {
   return (
     <>
       <Header/>
+      <List/>
       <Footer/>
     </>
   )
